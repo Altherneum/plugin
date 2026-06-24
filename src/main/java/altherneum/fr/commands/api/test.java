deb0bcc3fd74b5ee3ef98d80ed16ee1bc1e98f70
lx78WyY0J5 <9j2k37st@gmail.com>
2026-06-24 19:07:28 +0200

    forgot to import classes

--- src/main/java/altherneum/fr/commands/api/test.java
+++ src/main/java/altherneum/fr/commands/api/test.java
@@ -5,6 +5,12 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 
+import org.bukkit.inventory.ItemStack;
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.datacomponent.item.CustomModelData;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.entity.Player;
+
 import java.util.ArrayList;
 import java.util.List;
 
