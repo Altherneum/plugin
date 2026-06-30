4f604df3f9b02704d5950bc3c2cc1211f2d37344
lx78WyY0J5 <9j2k37st@gmail.com>
2026-06-30 16:35:45 +0200

    rm use less import

--- src/main/java/altherneum/fr/commands/api/test.java
+++ src/main/java/altherneum/fr/commands/api/test.java
@@ -5,12 +5,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 
-import org.bukkit.inventory.ItemStack;
-import io.papermc.paper.datacomponent.DataComponentTypes;
-import io.papermc.paper.datacomponent.item.CustomModelData;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.entity.Player;
-import org.bukkit.Material;
 import altherneum.fr.menu.api.customModel;
 
 import java.util.ArrayList;
